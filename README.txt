051ce87c70e79c1b09d9e334d743bfdbf4c03610
fram-vad <duyva.411@gmail.com>
2014-09-26 15:53:01 +0700

    Version 1
    First version

---
+++ README.txt
@@ -0,0 +1,32 @@
+=============================== 
+==== XAMPP DOMAIN CREATOR  ====
+=============================== 
+
+Created by Jimmy Hoang
+-------------------------
+
+I made this script to hep you create a virtual domain easier. 
+
+It is a batch file that prompt you to input name then it will create a folder with the same name in htdocs folder. It also add config in httpd-vhosts.conf and modify the hosts file.
+
+Default suffix is "dev" so if you enter jimmy as folder name you will have new virtual domain http://jimmy.dev You can change the suffix by edit line 5 in this script: SET SUFFIX="dev"
+
+============================================================================
+
+Thank you for downloading the Xampp Domain Creator. I hope it helps you to streamline
+web development workflow. 
+
+
+
+INSTALLATION
+============================================================================
+
+It is very easy, just copy create_domain.bat file into your XAMPP installed directory.
+
+
+
+USING
+============================================================================
+
+You can open a commandline window then change to XAMPP installed directory and run create_domain.bat.bat file
+Or open XAMPP shell window and run create_domain command.
---
+++ create_domain.bat
@@ -0,0 +1,56 @@
+@echo off
+cd /D %~dp0
+SET XAMPPDIR=%~dp0
+SET BASEURL=%XAMPPDIR%htdocs\
+SET SUFFIX="dev"
+
+@echo off
+echo ####### XAMPP Domain Creator v1.0 (c) Jimmy Hoang ######
+echo:
+echo:
+@ECHO OFF
+:sitenameprompt
+::ask for a sitename
+set /p sitename="Enter Sitename: " %=%
+
+IF EXIST %BASEURL%%sitename% echo The specified site folder already exists! Choose another. 
+IF EXIST %BASEURL%%sitename% goto sitenameprompt 
+
+IF NOT EXIST %BASEURL%%sitename% echo Creating folder 
+IF NOT EXIST %BASEURL%%sitename% MD %BASEURL%%sitename%
+
+echo Adding virtualhost to httpd.conf
+@echo off
+(
+echo:
+echo:
+echo    ###%sitename%###
+echo    ^<VirtualHost *:80^>
+echo        ServerAdmin admin@%sitename%
+echo        DocumentRoot "%BASEURL%%sitename%"
+echo        ServerName %sitename%.%SUFFIX%
+echo        ServerAlias %sitename%.%SUFFIX%
+echo        ^<Directory "%BASEURL%%sitename%"^>
+echo            Options Indexes FollowSymLinks Includes ExecCGI
+echo            Order allow,deny
+echo            Allow from all
+echo        ^</Directory^>
+echo    ^</VirtualHost^>
+echo    ^<VirtualHost *:443^>
+echo        SSLEngine on
+echo        SSLCertificateFile conf/ssl.crt/server.crt
+echo        SSLCertificateKeyFile conf/ssl.key/server.key 
+echo        DocumentRoot "%BASEURL%%sitename%"
+echo        ServerName %sitename%.%SUFFIX%
+echo        ServerAlias %sitename%.%SUFFIX% 
+echo        ^<IfModule alias_module^>
+echo            ScriptAlias /cgi-bin/ "%BASEURL%%sitename%/cgi-bin/"
+echo        ^</IfModule^>
+echo    ^</VirtualHost^>
+) >>%XAMPPDIR%apache\conf\extra\httpd-vhosts.conf
+pause
+echo Write into hosts file
+type "%SystemRoot%\system32\drivers\etc\hosts" | find "127.0.0.1 %sitename%.%SUFFIX%" ||echo.127.0.0.1 %sitename%.%SUFFIX%>>"%SystemRoot%\system32\drivers\etc\hosts"
+pause
+echo Restarting apache
+%XAMPPDIR%apache\bin\httpd -k restart
